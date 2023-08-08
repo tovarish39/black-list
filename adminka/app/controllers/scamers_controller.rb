3363cb57598081c4effa08dbad758fa0f6854760
gorrus39 <gorrus100@gmail.com>
2023-08-08 12:54:29 +0200

    fix scamers controller

--- adminka/app/controllers/scamers_controller.rb
+++ adminka/app/controllers/scamers_controller.rb
@@ -7,8 +7,10 @@ class ScamersController < ApplicationController
     @scamers = scamers.map do |user|
       complaint = Complaint.where(telegram_id:user.telegram_id).where(status:'accepted_complaint').first
       user_data = user.as_json
-      user_data['telegraph_link'] = complaint.telegraph_link  # Замените на реальное значение
-      user_data['telegram_link'] = "#{ENV['TELEGRAM_CHANNEL_USERNAME']}/#{complaint.mes_id_published_in_channel}"
+      if complaint.present?
+        user_data['telegraph_link'] = complaint.telegraph_link 
+        user_data['telegram_link'] = "#{ENV['TELEGRAM_CHANNEL_USERNAME']}/#{complaint.mes_id_published_in_channel}"
+      end
       user_data
     end
   end
