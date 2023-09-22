39346c68926e82b14652def97a0ce76671570b9a
gorrus39 <gorrus100@gmail.com>
2023-09-22 12:59:03 +0200

    fix

--- bot-main/controller.rb
+++ bot-main/controller.rb
@@ -75,7 +75,7 @@ def handle
         $user.update(chat_member_status: $mes.new_chat_member.status ) if $mes.new_chat_member.status.present?
       elsif new_private_channel_video?()
         write_video()
-      elsif !$lg; # заглушка   
+      elsif !$mes.from; # заглушка   
       elsif !is_bot_administrator_of_channel? # сообщение себе
       elsif user_is_blocked_by_moderator? 
       elsif !user_is_member_of_channel? && $lg.present? # если выбран язык, но не подписан на канал
