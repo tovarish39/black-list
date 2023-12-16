29a3530e786339f81112747da2ed011f7f9c7f0b
gorrus39 <gorrus100@gmail.com>
2023-12-17 00:03:55 +0200

    fix

--- start-bot-main.rb
+++ start-bot-main.rb
@@ -9,13 +9,32 @@ $logger.formatter = proc do |severity, datetime, _progname, msg|
   "#{severity.slice(0)} date=[#{date_format}] pid=##{Process.pid} message='#{msg}'\n"
 end
 
+
+# def get_logger(_user)
+#   log_path = "#{ENV['GARANT_ROOT_PATH']}/logs/#{$user.telegram_id}_#{$user.username}.log"
+#   logger = Logger.new(log_path, 'monthly')
+#   logger.formatter = proc do |severity, datetime, _progname, msg|
+#     date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
+#     "#{severity.slice(0)} [#{date_format}] pid=#{Process.pid} data='#{msg}'\n"
+#   end
+#   logger
+# end
+
+
+
+
 Telegram::Bot::Client.run(ENV['TOKEN_MAIN']) do |bot|
   bot.listen do |message|
     $bot = bot 
     $mes = message 
+
+
+
+
     begin
       handle if $mes
     rescue  => e
+      $logger.info("ERROR  #{$mes.inspect}")
       Send.mes(e, to: ENV['CHAT_ID_MY'])
       Send.mes(e.backtrace, to: ENV['CHAT_ID_MY'])
     end
