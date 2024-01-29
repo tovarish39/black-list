b13e50ef0a23e318608fbc8b91d4d33263ca7bd2
gorrus39 <gorrus100@gmail.com>
2024-01-29 20:26:34 +0200

    fix

--- bot-main/controller_helpers.rb
+++ bot-main/controller_helpers.rb
@@ -4,7 +4,7 @@ module ControllerHelpers # rubocop:disable Style/Documentation
   class << self
     def view_oracle_tips
       oracles_tips = Video.last.oracles_tips
-      if oracles_tips.present?
+      if !oracles_tips.nil?
         type = oracles_tips['type']
         file_id = oracles_tips['file_id']
         BotMain.send("send_#{type}", caption: Text.oracle_tips, reply_markup: M::Inline.link_to_oracles_tips, file_id:)
